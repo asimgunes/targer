8f08abba924e3c26321850095f45b053553048bd
Asım Güneş <asim@Asms-MacBook-Pro.local>
2019-12-21 19:46:16 +0300

    cuda optimization

--- src/layers/layer_context_word_embeddings.py
+++ src/layers/layer_context_word_embeddings.py
@@ -14,9 +14,9 @@ class LayerContextWordEmbeddings(LayerBase):
         print("Loading ELMo weights...")
         options_file = "embeddings/newscor.lower.elmo.options.json"
         weight_file = "embeddings/newscor.lower.elmo.weights.hdf5"
-        device = torch.device("cuda:"+gpu if (torch.cuda.is_available() and gpu > -1) else "cpu")        
+        device = torch.device("cuda:"+str(gpu) if (torch.cuda.is_available() and gpu > -1) else "cpu")        
         self.elmo = Elmo(options_file, weight_file, 2, dropout=0)
-        elmo = elmo.to(device) # Using cuda
+        self.elmo = self.elmo.to(device) # Using cuda
         print("ELMo weights loaded")
 
 
@@ -26,6 +26,9 @@ class LayerContextWordEmbeddings(LayerBase):
     def forward(self, word_sequences):
         #print("Creating ELMo weights...", word_sequences.shape)
         character_ids = batch_to_ids(word_sequences)
+        if(self.gpu > -1):
+            device = torch.device("cuda:"+str(self.gpu) if (torch.cuda.is_available() and self.gpu > -1) else "cpu")        
+            character_ids = character_ids.to(device)
         embeddings = self.elmo(character_ids)
         word_embeddings_feature = embeddings['elmo_representations'][0]
         #print("ELMo weights created")
